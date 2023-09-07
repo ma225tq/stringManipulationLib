ac5e778d49efb74b66cc5cf75626f3ccbcf7d504
“MelkerAndreasson” <ma22tq@student.lnu.se>
2023-09-07 03:09:15 +0200

    Add numberedList function

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ const capitalizeFirstLetter = require('../src/capitalizeFirstLetter')
 const truncate = require('../src/truncate')
 const removeChar = require('../src/removeChar')
 const lineBreakAfterEachWord = require('../src/lineBreakAfterEachWord')
-
+const numberedList = require('../src/numberedList')
 
---
+++ src/numberedList.js
@@ -0,0 +1,11 @@
+function numberedList(string) {
+    if (typeof string !== 'string') {
+        throw new Error('Input must be a string')
+    }
+
+    return string.split(' ')
+    .map((word, index) => `${index + 1}. ${word}`)
+    .join('\n');
+}
+
+module.exports = numberedList;
