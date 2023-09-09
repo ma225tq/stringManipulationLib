afdc5dd263748e767cde415d5b55c988c7041a1f
“MelkerAndreasson” <ma22tq@student.lnu.se>
2023-09-10 00:01:01 +0200

    add countWords function

---
+++ src/countWords.js
@@ -0,0 +1,8 @@
+function countWords(string) {
+  if (typeof string !== 'string') {
+    throw new Error('Input must be a string')
+  }
+  return string.split(' ').length
+}
+
+module.exports = countWords
